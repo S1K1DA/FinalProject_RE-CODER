05b63635360b1cd4956f1d0e4931dacc09bbcc30
PurPleChEEzE <ahssa96@gmail.com>
2024-08-13 17:31:06 +0900

    fix : 코드 수정하면서 누락됬던 부분 수정

--- src/main/java/com/heartlink/mypage/controller/MypageController.java
+++ src/main/java/com/heartlink/mypage/controller/MypageController.java
@@ -45,6 +45,61 @@ public class MypageController {
         return "mypage/mypage_main/mypage-infoedit";
     }
 
+    @GetMapping("/feedlike")
+    public String feedlikepage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_feedlike/mypage-feedlike";
+    }
+
+    @GetMapping("/ptreview")
+    public String ptreview(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_review/mypage-ptreview";
+    }
+
+    @GetMapping("/lireview")
+    public String lireview(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_review/mypage-lireview";
+    }
+
+    @GetMapping("/proflike")
+    public String profPage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_proflike/mypage-proflike";
+    }
+
+    @GetMapping("/match")
+    public String matchPage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_match/mypage-match";
+    }
+
+    @GetMapping("/delete")
+    public String deletePage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_delete/mypage-delete";
+    }
+
+    @GetMapping("/hobby")
+    public String hobbyPage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_hobby/mypage-hobby";
+    }
+
+    @GetMapping("/sentiedit")
+    public String sentiEditPage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_hobby/mypage-sentiedit";
+    }
+
+    @GetMapping("/hobbyedit")
+    public String hobbyEditPage(HttpServletRequest request, Model model) {
+        model.addAttribute("currentUrl", request.getRequestURI());
+        return "mypage/mypage_hobby/mypage-hobbyedit";
+    }
+
+
     // 비밀번호 확인 요청 처리
     @PostMapping("/validatePassword")
     @ResponseBody
