a9e56d53649dcc53db393df8b29c39569ef2f09e
PurPleChEEzE <ahssa96@gmail.com>
2024-08-14 14:58:12 +0900

    chore : 주석 추가

--- src/main/java/com/heartlink/mypage/model/service/MypageServiceImpl.java
+++ src/main/java/com/heartlink/mypage/model/service/MypageServiceImpl.java
@@ -15,47 +15,55 @@ public class MypageServiceImpl implements MypageService {
         this.mypageDao = mypageDao;
     }
 
+    //유저 정보 가져오기
     @Override
     public MypageDto getUserInfo(int userId) {
         return mypageDao.getUserInfoById(userId);
     }
 
+    //userId의 패스워드 가져오기
     @Override
     public String getPasswordByUserId(int userId) {
         return mypageDao.getPasswordByUserId(userId);
     }
 
+    //유저 정보 업데이트
     @Override
     public int updateUserInfo(MypageDto user) {
         return mypageDao.updateUserInfo(user);
     }
 
+    //타입별 성향 리스트 가져오기
     @Override
     public List<MypageDto> getPersonalCategoriesByType(String type) {
         return mypageDao.getPersonalCategoriesByType(type);
     }
 
+    //유저의 성향 리스트 가져오기
     @Override
     public List<Integer> getUserSelectedCategories(int userId) {
         return mypageDao.getUserSelectedCategories(userId);
     }
 
+    //유저의 성향 리스트 저장
     @Override
     public void saveUserCategories(int userId, List<Integer> categoryIds) {
         mypageDao.saveUserCategories(userId, categoryIds);
     }
 
-    // 추가된 메소드들
+    //취미 리스트 가져오기
     @Override
     public List<MypageDto> getHobbyCategories() {
         return mypageDao.getHobbyCategories();
     }
 
+    //유저 취미 리스트 저장
     @Override
     public void saveUserHobbies(int userId, List<Integer> hobbyIds) {
         mypageDao.saveUserHobbies(userId, hobbyIds);
     }
 
+    //유저 취미 리스트 가져오기
     @Override
     public List<MypageDto> getUserHobbies(int userId) {
         return mypageDao.getUserHobbies(userId);
