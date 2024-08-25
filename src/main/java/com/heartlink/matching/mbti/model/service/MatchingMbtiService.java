1238c7c89a169dd710202bd63e1d00c42fe1bf37
eunsik jung <bamtol2100@naver.com>
2024-08-25 17:00:02 +0900

    feat : MBTI 매칭 페이지 - 프론트 아이콘,폰트 추가   MBTI별 천생연분 값 불러오기
    - 백엔드 닉네임, mbti값 불러오기.

--- src/main/java/com/heartlink/matching/mbti/model/service/MatchingMbtiService.java
+++ src/main/java/com/heartlink/matching/mbti/model/service/MatchingMbtiService.java
@@ -1,4 +1,22 @@
 package com.heartlink.matching.mbti.model.service;
 
+import com.heartlink.matching.mbti.model.dto.MatchingMbtiDto;
+import com.heartlink.matching.mbti.model.mapper.MatchingMbtiMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MatchingMbtiService {
+
+    private final MatchingMbtiMapper matchingMbtiMapper;
+
+    @Autowired
+    public MatchingMbtiService(MatchingMbtiMapper matchingMbtiMapper) {
+        this.matchingMbtiMapper = matchingMbtiMapper;
+    }
+
+    // 특정 사용자의 프로필 가져오기
+    public MatchingMbtiDto getUserProfile(int userNumber) {
+        return matchingMbtiMapper.getUserProfileById(userNumber);
+    }
 }
