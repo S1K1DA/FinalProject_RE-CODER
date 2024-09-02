6866787ea6eb4ac679f6f413004f063de00e7cb4
JaeIn <73939275+popya-02@users.noreply.github.com>
2024-09-02 15:22:04 +0900

    chore : 위치기반 매칭 프로필 이미지 존재하지 않을시 기본 이미지 할당

--- src/main/java/com/heartlink/HeartLinkApplication.java
+++ src/main/java/com/heartlink/HeartLinkApplication.java
@@ -7,11 +7,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class HeartLinkApplication {
-
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
     }
-
     public static void main(String[] args) {
         SpringApplication.run(HeartLinkApplication.class, args);
     }
